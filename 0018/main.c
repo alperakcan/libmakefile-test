2179b6fffe1e400c5b0cc362d8de576c87a8490c
Alper Akcan <alper.akcan@gmail.com>
2026-08-16 23:04:50 +0300

    update

---
+++ 0018/main.c
@@ -0,0 +1,9 @@
+
+#include "version.h"
+
+int main (void)
+{
+        const char * v = version();
+        (void) v;
+        return 0;
+}
---
+++ 0018/version.c
@@ -0,0 +1,5 @@
+
+const char * version (void)
+{
+        return "version";
+}
